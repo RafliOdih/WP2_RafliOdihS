65d1cb929e3542a4743320c39f02cd86b4d11c09
Rafli Odih Saputra <90746377+RafliOdih@users.noreply.github.com>
2021-09-21 12:55:44 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class Contoh1 extends BaseController
+{
+    public function index()
+    {
+        echo "<p><h1>Perkenalkan</h1></p>";
+        echo "<p>Nama saya Rafli Odih Saputra</p> 
+              <p>Saya tinggal di Bekasi , Pondok Melati</p>
+              <p>Hobi saya adalah Bermain Game</p>";
+    }
+}
