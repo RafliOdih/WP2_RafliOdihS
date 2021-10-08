9d20c70f3030fc61bf0ed045d633a8d8b281aa5f
Rafli Odih Saputra <90746377+RafliOdih@users.noreply.github.com>
2021-10-08 20:37:19 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Rafli</li>
+        <li>Nama Belakang : Odih Saputra</li>
+        <li>Nama Lengkap : Rafli Odih Saputra</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="square">
+        <li> Tevyat , Liyue, No.30</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="square">
+        <li> Planet tevyat</li>
+    </ul>
+    <h4>Hobby</h4>
+    <ul type="square">
+        <li>Ngegame</li>
+        <li>Dengar Music</li>
+    </ul>
+</section>
